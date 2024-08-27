281ed304818634fe66c6ba916416dd279fcd2f47
Samuvelajay <118651367+Samuvelajay@users.noreply.github.com>
2024-08-27 11:24:50 +0530

    Add files via upload

---
+++ src/index.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import './index.css';
+//import Timer from './Components/Timer';
+import reportWebVitals from './reportWebVitals';
+//import Myfrom from './Components/Myfrom'
+import App from './App';
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
+
+reportWebVitals();
